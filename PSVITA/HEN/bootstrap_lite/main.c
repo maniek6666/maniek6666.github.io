89c78a1ce51efed87e8291b6e15e8a93e9873612
maniek6666 <79801381+maniek6666@users.noreply.github.com>
2022-12-28 19:32:39 +0100

    Add files via upload

--- PSVITA/HEN/bootstrap_lite/main.c
+++ PSVITA/HEN/bootstrap_lite/main.c
@@ -68,7 +68,7 @@ enum E_MENU_OPTIONS {
     MENU_REPLACE_NEAR,
     MENU_RESET_TAICFG
 };
-const char* menu_items[OPTION_COUNT] = { " -> Exit", " -> Install henkaku", " -> Install VitaDeploy", " -> Replace NEAR with VitaDeploy", " -> Reset taihen config.txt" };
+const char* menu_items[OPTION_COUNT] = { " -> Wyjdz", " -> Zainstaluj henkaku", " -> Zainstaluj VitaDeploy", " -> Zastap NEAR na VitaDeploy", " -> Resetowanie taihen config.txt" };
 
 int __attribute__((naked, noinline)) call_syscall(int a1, int a2, int a3, int num) {
     __asm__(
@@ -88,35 +88,35 @@ int unzip(const char* src, const char* dst) {
 
 int install_vitadeploy_default() {
     sceIoMkdir(TEMP_UX0_PATH, 0777);
-    COLORPRINTF(COLOR_CYAN, "Downloading vitadeploy\n");
+    COLORPRINTF(COLOR_CYAN, "Pobieranie vitadeploy\n");
     net(1);
     int res = download_file(HEN_REPO_URL VDEP_VPK_FNAME, TEMP_UX0_PATH VDEP_VPK_FNAME, TEMP_UX0_PATH VDEP_VPK_FNAME "_tmp", 0);
     if (res < 0)
         return res;
     net(0);
 
-    COLORPRINTF(COLOR_CYAN, "Extracting vpk\n");
+    COLORPRINTF(COLOR_CYAN, "Wypakowywanie vpk\n");
     remove(TEMP_UX0_PATH "app");
     sceIoMkdir(TEMP_UX0_PATH "app", 0777);
     res = unzip(TEMP_UX0_PATH VDEP_VPK_FNAME, TEMP_UX0_PATH "app");
     if (res < 0)
         return res;
 
-    COLORPRINTF(COLOR_CYAN, "Promoting app\n");
+    COLORPRINTF(COLOR_CYAN, "Promowanie app\n");
     res = promoteApp(TEMP_UX0_PATH "app");
     if (res < 0)
         return res;
 
-    COLORPRINTF(COLOR_GREEN, "All done\n");
+    COLORPRINTF(COLOR_GREEN, "Wszystko gotowe \n");
     sceKernelDelayThread(2 * 1000 * 1000);
 
     return 0;
 }
 
 int vitadeploy_x_near(int syscall_id) {
-    COLORPRINTF(COLOR_WHITE, "This replaces the discontinued \"near\" app with VitaDeploy.\nRequires reboot to take effect.\n\n");
-    COLORPRINTF(COLOR_YELLOW, "WARNING: this will cause your bubble layout to be reset\n\n");
-    COLORPRINTF(COLOR_CYAN, "SQUARE: Replace NEAR with VitaDeploy\nTRIANGLE: Restore NEAR\nCIRCLE: Go back\n");
+    COLORPRINTF(COLOR_WHITE, "Spowoduje to zastapienie aplikacji \"near\" na aplikacje VitaDeploy.\nWymaga ponownego uruchomienia.\n\n");
+    COLORPRINTF(COLOR_YELLOW, "OSTRZEZENIE: spowoduje to zresetowanie ukLad babeli\n\n");
+    COLORPRINTF(COLOR_CYAN, "KWADRAT: Zmien NEAR na VitaDeploy\nTROJKAT: Przywroc NEAR\nKOLKO: Wroc\n");
     sceKernelDelayThread(0.5 * 1000 * 1000);
     SceCtrlData pad;
     while (1) {
@@ -126,69 +126,69 @@ int vitadeploy_x_near(int syscall_id) {
         else if (pad.buttons == SCE_CTRL_CIRCLE)
             return 0;
         else if (pad.buttons == SCE_CTRL_TRIANGLE) {
-            printf("Preparing to restore NEAR..\n");
+            printf("Przygotowanie do przywrocenia NEAR..\n");
             sceIoMkdir(TEMP_UR0_PATH, 0777);
             removeDir(TEMP_UR0_PATH "app");
             sceIoMkdir(TEMP_UR0_PATH "app", 0777);
             int ret = copyDir("vs0:app/NPXS10000/near_backup", TEMP_UR0_PATH "app");
             if (ret < 0)
                 return ret;
-            printf("Removing VitaDeploy..\n");
+            printf("Usuwanie VitaDeploy..\n");
             ret = call_syscall(0, 0, 0, syscall_id + 4);
             cprintf("ioMountRW ret 0x%08X\n", ret);
             ret = removeDir("vs0:app/NPXS10000");
             if (ret < 0) {
-                printf("Failed 0x%08X, rebooting in 5s\n", ret);
+                printf("Blad 0x%08X, ponowne uruchomienie za 5 sekund\n", ret);
                 sceKernelDelayThread(5 * 1000 * 1000);
                 scePowerRequestColdReset();
                 sceKernelDelayThread(2 * 1000 * 1000);
                 printf("Process is dead\n");
                 while (1) {};
             }
-            printf("Restoring NEAR..\n");
+            printf("Przywracanie NEAR..\n");
             ret = copyDir(TEMP_UR0_PATH "app", "vs0:app/NPXS10000");
             if (ret < 0)
                 printf("Failed 0x%08X\n", ret);
-            printf("Removing the application database\n");
+            printf("Usuwanie bazy danych aplikacji\n");
             sceIoRemove("ur0:shell/db/app.db");
-            printf("Cleaning up..\n");
+            printf("Czyszczenie..\n");
             removeDir(TEMP_UR0_PATH "app");
             printf("All done, rebooting in 5s\n");
             sceKernelDelayThread(5 * 1000 * 1000);
             scePowerRequestColdReset();
             sceKernelDelayThread(2 * 1000 * 1000);
-            printf("Process is dead\n");
+            printf("Proces nie powiodl sie\n");
             while (1) {};
         }
     }
     sceIoMkdir(TEMP_UR0_PATH, 0777);
-    printf("Downloading vitadeploy\n");
+    printf("Pobieranie vitadeploy\n");
     net(1);
     int res = download_file(HEN_REPO_URL VDEP_VPK_FNAME, TEMP_UR0_PATH VDEP_VPK_FNAME, TEMP_UR0_PATH VDEP_VPK_FNAME "_tmp", 0);
     if (res < 0)
         return res;
     net(0);
 
-    printf("Extracting vpk\n");
+    printf("Wypakowywanie vpk\n");
     removeDir(TEMP_UR0_PATH "app");
     sceIoMkdir(TEMP_UR0_PATH "app", 0777);
     res = unzip(TEMP_UR0_PATH VDEP_VPK_FNAME, TEMP_UR0_PATH "app");
     if (res < 0)
         return res;
 
-    printf("Backing up NEAR\n");
+    printf("Tworzenie kopii zapasowej NEAR\n");
     res = copyDir("vs0:app/NPXS10000", TEMP_UR0_PATH "app/near_backup");
     if (res < 0)
         return res;
 
-    printf("Preparing to replace near..\n");
+    printf("Przygotowanie do podmiany near..\n");
     sceIoRemove(TEMP_UR0_PATH "app/sce_sys/param.sfo");
     sceIoRename(TEMP_UR0_PATH "app/sce_sys/vs.sfo", TEMP_UR0_PATH "app/sce_sys/param.sfo");
     
     res = call_syscall(0, 0, 0, syscall_id + 4);
     cprintf("ioMountRW ret 0x%08X\n", res);
 
-    printf("Replacing NEAR\n");
+    printf("Podmiana NEAR\n");
     res = removeDir("vs0:app/NPXS10000");
     cprintf("remove near : 0x%08X\n", res);
 
@@ -196,13 +196,13 @@ int vitadeploy_x_near(int syscall_id) {
     if (res < 0)
         return res;
 
-    printf("Removing the application database\n");
+    printf("Usuwanie bazy danych aplikacji\n");
     sceIoRemove("ur0:shell/db/app.db");
 
-    printf("Cleaning up..\n");
+    printf("Czyszczenie..\n");
     removeDir(TEMP_UR0_PATH "app");
 
-    printf("All done, rebooting in 5s\n");
+    printf("Wszystko gotowe, ponowne uruchomienie za 5 sekund\n");
     sceKernelDelayThread(5 * 1000 * 1000);
     scePowerRequestColdReset();
     sceKernelDelayThread(2 * 1000 * 1000);
@@ -225,11 +225,11 @@ int install_henkaku(void) {
         sceIoClose(fd);
         already_taid = 1;
     }
-    COLORPRINTF(COLOR_CYAN, "Downloading the latest taihen build\n");
+    COLORPRINTF(COLOR_CYAN, "Pobieranie najnowszej wersji taihen\n");
     int ret = download_file(HEN_REPO_URL TAIHEN_K_FNAME, "ur0:tai/" TAIHEN_K_FNAME, "ur0:tai/" TAIHEN_K_FNAME "_tmp", 0);
     if (ret < 0)
         return ret;
-    COLORPRINTF(COLOR_CYAN, "Downloading the latest henkaku builds\n");
+    COLORPRINTF(COLOR_CYAN, "Pobieranie najnowszych kompilacji henkaku\n");
     ret = download_file(HEN_REPO_URL HENKAKU_K_FNAME, "ur0:tai/" HENKAKU_K_FNAME, "ur0:tai/" HENKAKU_K_FNAME "_tmp", 0);
     if (ret < 0)
         return ret;
@@ -237,12 +237,12 @@ int install_henkaku(void) {
     if (ret < 0)
         return ret;
     if (!already_taid) {
-        COLORPRINTF(COLOR_CYAN, "Downloading the default taihen config\n");
+        COLORPRINTF(COLOR_CYAN, "Pobieranie domyslnego taihen config\n");
         ret = download_file(HEN_REPO_URL TAIHEN_C_FNAME, "ur0:tai/" TAIHEN_C_FNAME, "ur0:tai/" TAIHEN_C_FNAME "_tmp", 0);
         if (ret < 0)
             return ret;
     }
-    COLORPRINTF(COLOR_GREEN, "All done\n");
+    COLORPRINTF(COLOR_GREEN, "Wszystko gotowe\n");
     sceKernelDelayThread(2 * 1000 * 1000);
     return 0;
 }
@@ -264,14 +264,14 @@ void main_menu(int sel) {
 int _start(SceSize args, void* argp) {
     int syscall_id = *(uint16_t*)argp;
 
-    cprintf("loading PAF\n");
+    cprintf("Ladowanie PAF\n");
     int res = load_sce_paf();
     if (res < 0)
         goto EXIT;
 
     psvDebugScreenInit();
     psvDebugScreenSetFgColor(COLOR_CYAN);
-    printf("henlo-bootstrap\n");
+    printf("henlo-bootstrap (Polski MOD Mr.NOBODY PS3)\n");
     psvDebugScreenSetFgColor(COLOR_YELLOW);
 
     int sel = 0;
@@ -361,19 +361,19 @@ EXIT:
     }
 
     if (res < 0 && res != 0x8002D013 && res != 0x8002D017) {
-        printf(" > Failed to load HENkaku! 0x%08X\n", res);
-        printf(" > Please relaunch the exploit and select 'Install HENkaku'.\n");
+        printf(" > Nie udalo sie zaladowac HENkaku! 0x%08X\n", res);
+        printf(" > Uruchom ponownie Exploita i wybierz 'Zainstaluj HENkaku'.\n");
     }
 
     // Clean up
-    cprintf("Cleanup.. \n");
+    cprintf("Czyszczenie.. \n");
     call_syscall(0, 0, 0, syscall_id + 3);
 
     cprintf("all done, exit\n");
-    printf("Exiting in 3\n");
+    printf("Wyjscie za 3 sekundy\n");
     sceKernelDelayThread(3 * 1000 * 1000);
 
-    cprintf("unloading PAF\n");
+    cprintf("rozladunek PAF\n");
     unload_sce_paf();
 
     sceKernelExitProcess(0);
